20ee91b351562cb8f45120a11246e8e2436edf58
Alex <alexxmish21@gmail.com>
2023-10-27 15:57:08 +0300

    Add file

---
+++ coolsite/students/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import *
+from .classurl import FourDigitYearConverter
+register_converter(FourDigitYearConverter, "yyyy")
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('students/<int:studid>/', students_id),
+    path('students/', students_mainpage, name='mpage'),
+    path('raise_err_400', e_400),
+    path('raise_err_500', e_500),
+    path('raise_err_403', e_403),
+    path('years/<yyyy:year_id>/', year),
+]
