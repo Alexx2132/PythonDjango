8bff25c49f18c0f06eaf6bbe2bcab4b42bc5447e
Alex <alexxmish21@gmail.com>
2023-10-29 18:21:07 +0300

    Add file дз на 31 окт

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)#&p_amlv+@604*5paerv-751-v+esiwbzr$-v7riq+pkk=8=h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
--- coolsite/students/urls.py
+++ coolsite/students/urls.py
@@ -7,7 +7,7 @@ register_converter(FourDigitYearConverter, "yyyy")
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('students/<int:studid>/', students_id),
+    path('students/<int:studid>/', students_id, name='stud_id'),
     path('students/', students_mainpage, name='mpage'),
     path('raise_err_400', e_400),
     path('raise_err_500', e_500),
